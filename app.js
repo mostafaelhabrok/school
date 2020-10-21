f7a0a85a11881aee66264a77d66531453cbfae5b
mostafa elhabrok <mostafa.elhabrok@gmail.com>
2020-10-21 14:13:29 +0200

    initial005

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 (function($){
+  if(document.cookie.split('=')[1]!='true'){
+    alert('Login first please');
+    location.href='index.html';};
+
     $(document).ready(function(){
-      
 $('#logout').click(function(){
   document.cookie = "login=;expires=Thu, 01 Jan 1970 00:00:00 UTC; path='/'";
 
